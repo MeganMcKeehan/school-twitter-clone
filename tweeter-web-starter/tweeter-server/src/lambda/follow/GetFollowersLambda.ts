57cb144c911c2660f85f6a00f40bd8f7f3f1885a
MeganMcKeehan <132294021+MeganMcKeehan@users.noreply.github.com>
2025-12-02 08:54:27 -0700

    update followers lambda

--- tweeter-web-starter/tweeter-server/src/lambda/follow/GetFollowersLambda.ts
+++ tweeter-web-starter/tweeter-server/src/lambda/follow/GetFollowersLambda.ts
@@ -1,21 +1,35 @@
-import { PagedUserItemRequest, PagedUserItemResponse } from "tweeter-shared";
+import {
+  PagedUserItemRequest,
+  PagedUserItemResponse,
+  UserDto,
+} from "tweeter-shared";
 import { FollowService } from "../../model/service/FollowService";
 import { DynamoDAOFactory } from "../../model/factories/dynamoFactory";
+import { UserService } from "../../model/service/UserService";
 
 export const handler = async (
   request: PagedUserItemRequest
 ): Promise<PagedUserItemResponse> => {
-  const followService = new FollowService(new DynamoDAOFactory());
+  const daoFactory = new DynamoDAOFactory();
+  const followService = new FollowService(daoFactory);
+  const userService = new UserService(daoFactory);
   const [items, hasMore] = await followService.loadMoreFollowers(
     request.token,
     request.userAlias,
     request.pageSize,
     request.lastItem
   );
+  const users: UserDto[] = [];
+  for (const item in items) {
+    const foundUser = await userService.getUser(request.token, item);
+    if (foundUser) {
+      users.push(foundUser);
+    }
+  }
   return {
     success: true,
     message: null,
-    items: items,
+    items: users,
     hasMore: hasMore,
   };
 };
